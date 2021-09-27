c023369e4c11a585146db089d245a82743409720
ni-xon <nixonuch_@hotmail.com>
2021-09-27 17:34:06 +1000

    Added dark mode functionality

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// DARK MODE BUTTON
+const root = document.querySelector("html");
+const darkModeButton = document.getElementById("theme-button");
+
+darkModeButton.addEventListener("click", () => {
+  root.classList.toggle("dark-mode");
+  if (root.classList.contains("dark-mode")) {
+    root.style.setProperty("--bg-color", "black");
+    root.style.setProperty("--text-color", "white");
+  } else if (!root.classList.contains("dark-mode")) {
+    root.style.setProperty("--bg-color", "white");
+    root.style.setProperty("--text-color", "black");
+  }
+});
