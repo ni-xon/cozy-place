be09509d92328a1e649feba0bf8bf70ce51837fb
ni-xon <nixonuch_@hotmail.com>
2021-09-28 11:39:54 +1000

    Added spotify icon and styled dark mode button

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ const darkModeButton = document.getElementById("theme-button");
 darkModeButton.addEventListener("click", () => {
   root.classList.toggle("dark-mode");
   if (root.classList.contains("dark-mode")) {
+    darkModeButton.textContent = "Light Mode";
     root.style.setProperty("--bg-color", "black");
     root.style.setProperty("--text-color", "white");
   } else if (!root.classList.contains("dark-mode")) {
+    darkModeButton.textContent = "Dark Mode";
     root.style.setProperty("--bg-color", "white");
     root.style.setProperty("--text-color", "black");
   }
