a854043bd337c479554ab2e1c87f894e89e9d544
ni-xon <nixonuch_@hotmail.com>
2021-09-28 14:14:58 +1000

    Added sun and moon animations and mobile display

--- js/main.js
+++ js/main.js
@@ -4,16 +4,23 @@ const darkModeButton = document.getElementById("theme-button");
 const spotifyButton = document.querySelector(".spotify-button");
 const spotifyEmbed = document.getElementById("spotify-embed");
 
+const moonIcon = document.querySelector(".fa-moon");
+const sunIcon = document.querySelector(".fa-sun");
+
 darkModeButton.addEventListener("click", () => {
   root.classList.toggle("dark-mode");
   if (root.classList.contains("dark-mode")) {
     darkModeButton.textContent = "Light Mode";
     root.style.setProperty("--bg-color", "black");
     root.style.setProperty("--text-color", "white");
+    sunIcon.style.transform = "translateY(110vh)";
+    moonIcon.style.transform = "translateY(-110vh)";
   } else if (!root.classList.contains("dark-mode")) {
     darkModeButton.textContent = "Dark Mode";
     root.style.setProperty("--bg-color", "white");
     root.style.setProperty("--text-color", "black");
+    moonIcon.style.transform = "translateY(0vh)";
+    sunIcon.style.transform = "translateY(0vh)";
   }
 });
 
